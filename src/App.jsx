3cb13df8610f5205ff8d22c2eb2cafc71a74fe5b
czhu-24 <blue.ink.stains@gmail.com>
2023-09-06 00:36:38 -0700

    first react commit phew

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+
+import HelloWorld from './components/HelloWorld.jsx';
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App;
+
+
